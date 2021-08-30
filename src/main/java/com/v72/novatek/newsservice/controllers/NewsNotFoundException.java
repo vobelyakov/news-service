0be44a08514982f5178c08c014277886a20f3392
Vasiliy Belyakov <v.belyakov@gmail.com>
2021-08-30 21:46:32 +0500

    set error code 404

--- src/main/java/com/v72/novatek/newsservice/controllers/NewsNotFoundException.java
+++ src/main/java/com/v72/novatek/newsservice/controllers/NewsNotFoundException.java
@@ -1,7 +1,10 @@
 package com.v72.novatek.newsservice.controllers;
 
-class NewsNotFoundException extends RuntimeException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(value= HttpStatus.NOT_FOUND, reason="No such News")
+class NewsNotFoundException extends RuntimeException {
     NewsNotFoundException(Long id) {
         super("Could not find news " + id);
     }
