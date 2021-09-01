2e45e6d5b2a2609bc2048f7474c046c55c955c79
Vasiliy Belyakov <v.belyakov@gmail.com>
2021-09-01 20:07:44 +0500

    add swagger

--- src/main/java/com/v72/novatek/newsservice/NewsServiceApplication.java
+++ src/main/java/com/v72/novatek/newsservice/NewsServiceApplication.java
@@ -4,6 +4,13 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.List;
 
 @SpringBootApplication
 public class NewsServiceApplication {
